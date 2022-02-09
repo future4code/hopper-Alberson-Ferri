23b6505fe618232757ef3d69f03eea729e587192
ferriqueiroz <lordaiox@gmail.com>
2022-02-08 21:33:39 -0400

    index.html

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,65 @@
+// Aula de Variaveis
+
+// 1 //
+
+//let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+
+// 10 5 sera o resultado
+
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+
+//console.log(a, b, c)
+
+// 10 10 10 sera o resultado
+
+//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+
+
+//const seuNome = prompt ("Qual Seu Nome")
+//const suaIdade = Number (prompt("Qual sua Idade"))
+
+//console.log(typeof seuNome)
+//console.log(typeof suaIdade)
+
+//console.log(valor1, valor2)
+
+//String Number
+
+//Foi impresso String por ser texto com aspas.
+
+// console.log("Ola",seuNome, "Voce tem",suaIdade, "anos")
+
+// 2 //
+
+//const aulaHoje = prompt ("A Aula foi Boa")
+//const diaHoje = prompt ("O seu Dia foi Bom")
+//const estaCansado = prompt ("Esta cansado Hoje")
+
+//console.log(typeof aulaHoje, typeof diaHoje, typeof estaCansado)
+
+// 3 //
+
+let a = 10
+let b = 25
+let c = 1
+
+c = a
+a = b
+b = c
+
+
+console.log(a,b)
+
