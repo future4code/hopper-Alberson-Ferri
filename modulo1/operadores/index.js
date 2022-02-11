461d67267d47ca0bf50a7d43d8c8ffc5efd75dba
ferriqueiroz <lordaiox@gmail.com>
2022-02-10 22:03:10 -0400

    index.html

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,32 @@
+/* 
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado) 
+*/
+
+/*
+let primeiroNumero = Number  (prompt ( "Digite um numero!" ))
+let segundoNumero = Number (prompt  ( "Digite outro numero!"))
+
+const soma = (primeiroNumero + segundoNumero)
+
+console.log(soma)
+*/
+
+let idadeUsuario = Number (prompt ("Qual Sua Idade?"))
+let idadeMelhor = Number (prompt ("Qual a Idade do Seu MElho Amigo?"))
+
+let resultado = idadeUsuario = idadeMelhor 
+
+
