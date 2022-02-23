8d3bef8a42299f1ed6701715c78411587115dfc6
ferriqueiroz <lordaiox@gmail.com>
2022-02-22 21:17:54 -0400

    objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,110 @@
+/*
+const filme = {
+	nome: "Auto da Compadecida", 
+	ano: 2000, 
+	elenco: [
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Virginia Cavendish"
+		], 
+	transmissoesHoje: [
+		{canal: "Telecine", horario: "21h"}, 
+		{canal: "Canal Brasil", horario: "19h"}, 
+		{canal: "Globo", horario: "14h"}
+		]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+
+const cachorro = {
+	nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+}
+
+const gato = {...cachorro, nome: "Juba"}
+
+const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+
+function minhaFuncao(objeto, propriedade) {
+	return objeto[propriedade]
+}
+
+const pessoa = {
+  nome: "Caio", 
+  idade: 23, 
+  backender: false
+}
+
+console.log(minhaFuncao(pessoa, "backender"))
+console.log(minhaFuncao(pessoa, "altura")) 
+*/
+
+// Exercico 1
+
+/*
+const pessoa = {
+    nome: "Carlos",
+    apelidos: ["Careca", "Miudo", "Doidao"]
+}
+
+const novoApelidos = pessoa.apelidos
+pessoa.apelidos = ["William", "Mateus", "Charles"]
+
+
+
+console.log ("Eu sou", pessoa.nome, "mas pode me chamar de", pessoa.apelidos)
+*/
+
+// Exercicio 2
+
+/*
+const pessoa1 = {
+    nome: "Arthur",
+    idade: 25,
+    profissao: "Medico"
+
+}
+
+var nome = "Arthur";
+var tamanho = nome.length;
+
+var profissao = "Medico";
+var tamanho = profissao.length;
+ 
+ console.log ([pessoa1.nome, nome.length, pessoa1.idade, pessoa1.profissao, profissao.length])
+ 
+
+const pessoa2 = {
+    nome: "Azaph",
+    idade: 32,
+    profissao: "Policial"
+
+}
+
+var nome = "Azaph";
+var tamanho = nome.length;
+
+var profissao = "Policial";
+var tamanho = profissao.length;
+ 
+
+console.log ([pessoa2.nome, nome.length, pessoa2.idade, pessoa2.profissao, profissao.length])
+*/
+ 
+// Exercico 3
+
+const array = {
+    nome: Banana,
+    disponibilidade: true
+}
+
+console.log ("nome", array.nome)
+
+
+
